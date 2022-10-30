42514912eaede030e3517e12bbfe914f96690bb6
REIGEN <daniil-yak@yandex.ru>
2022-10-30 22:03:57 +0300

    ListPeople and GetPeopleByID

--- service/store/store.go
+++ service/store/store.go
@@ -3,7 +3,6 @@ package store
 import (
 	"context"
 	"fmt"
-	"os"
 
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
@@ -15,8 +14,8 @@ type Store struct {
 }
 
 type People struct {
-	ID   int
-	Name string
+	ID   int    `db:"id"`
+	Name string `db:"name"`
 }
 
 // NewStore creates new database connection
@@ -27,13 +26,13 @@ func NewStore(connString string) *Store {
 	}
 
 	driver, err := postgres.WithInstance(conn.DB, &postgres.Config{})
-	if err != nil { //Ругается, если ничего не делаю с err, но нужны ли они тут?
+	if err != nil {
 		panic(err)
 	}
 	m, err := migrate.NewWithDatabaseInstance(
 		"../../migrations/1_initial.up.sql",
 		"postgres", driver)
-	if err != nil { //и тут/////////
+	if err != nil {
 		panic(err)
 	}
 	m.Up()
@@ -43,34 +42,40 @@ func NewStore(connString string) *Store {
 	}
 }
 
-func (s *Store) ListPeople() ([]People, error) {
-
-	rows, err := s.conn.Query(context.Background(), `
-	SELECT name, address
-	FROM client
-	`)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Query failed: %v\n", err)
-		return nil, nil
-	}
-	defer rows.Close()
+func (s *Store) ListPeople(ctx context.Context) ([]People, error) {
+	//rows, err := s.conn.QueryContext(ctx, `SELECT name, address FROM people`)
+	//defer rows.Close()
 	people := make([]People, 0) //слайс со всеми людьми
-	for rows.Next() {
-		var (
-			name    string
-			address string
-		)
-
-		if err := rows.Scan(&name, &address); err != nil {
-			fmt.Fprintf(os.Stderr, "scan failed: %v\n", err)
-			return nil, nil
-		}
-		people = append(people, rows.Scan(&name, &address))
-
+	err := s.conn.SelectContext(ctx, &people, `SELECT name, address FROM people`)
+	if err != nil {
+		return nil, fmt.Errorf("store ListPeople error: %w", err)
 	}
+
+	// if err != nil {
+	// 	fmt.Fprintf(os.Stderr, "Query failed: %v\n", err)
+	// 	return nil, nil
+	// }
+	// for rows.Next() {
+	// 	var (
+	// 		name    string
+	// 		address string
+	// 	)
+
+	// 	if err := rows.Scan(&name, &address); err != nil {
+	// 		fmt.Fprintf(os.Stderr, "scan failed: %v\n", err)
+	// 		return nil, nil
+	// 	}
+	// 	people = append(people, &name, &address)
+
+	// }
 	return people, err
 }
 
-func (s *Store) GetPeopleByID(id string) (People, error) {
-	return People{}, nil
+func (s *Store) GetPeopleByID(ctx context.Context, id string) (People, error) {
+	var pp People
+	err := s.conn.SelectContext(ctx, pp, `SELECT name, address FROM people WHERE People.ID = id`)
+	if err != nil {
+		return nil, fmt.Errorf("store GetPeopleByID error: %w", err)
+	}
+	return pp, nil
 }
